828f921521cb6f79b31143a97a90462f7c9cb30f
Blossom Ghuntla <bghuntla123@gmail.com>
2021-10-11 21:06:46 +0530

    cols and rows created with insert method, config updated

--- tablemodel.py
+++ tablemodel.py
@@ -2,18 +2,21 @@ from PyQt5 import QtCore
 
 
 class TableModel(QtCore.QAbstractTableModel):
-	def __init__(self, cols_strings, rows_strings, empty_strings, parent=None):
+	def __init__(self, head_columns_names, head_rows_names, table_rows_count=int, table_columns_count=int, parent=None):
 		super(TableModel, self).__init__(parent)
 
-		self.colStringsList = cols_strings
-		self.rowStringsList = rows_strings
-		self.data = empty_strings
+		self.headColStringsList = head_columns_names
+		self.headRowStringsList = head_rows_names
+		self.data = []  # this is just an empty data to use for row
+		self.data_column = []  # this is just an empty data to use for column because
+		self.table_rows_count = table_rows_count
+		self.table_columns_count = table_columns_count
 
-	def rowCount(self, index=QtCore.QModelIndex()):
+	def rowCount(self, parent=QtCore.QModelIndex):
 		return len(self.data)
 
-	def columnCount(self, index=QtCore.QModelIndex()):
-		return len(self.data)
+	def columnCount(self, parent=QtCore.QModelIndex()):
+		return len(self.data_column)
 
 	def data(self, index=QtCore.QModelIndex, role=QtCore.Qt.DisplayRole):
 		if index.isValid():
@@ -30,9 +33,31 @@ class TableModel(QtCore.QAbstractTableModel):
 	def headerData(self, section, orientation=QtCore.Qt.Orientation, role=QtCore.Qt.DisplayRole):
 		if role == QtCore.Qt.DisplayRole:
 			if orientation == QtCore.Qt.Horizontal:
-				return self.colStringsList[section]
+				return self.headColStringsList[section]
 			if orientation == QtCore.Qt.Vertical:
-				return self.rowStringsList[section]
+				return self.headRowStringsList[section]
 
 	def flags(self, index):
 		return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable
+
+	def insertRow(self, position, index=QtCore.QModelIndex()):
+		self.insertRows(position, self.table_rows_count, index)
+
+	def insertRows(self, position, rows, parent=QtCore.QModelIndex()):
+		self.beginInsertRows(parent, position, position + rows - 1)
+		for row in range(0, rows):
+			# mention rows as per the column count
+			self.data.insert(position, ["", "", "", "",""])
+		self.endInsertRows()
+		return True
+
+	def insertColumn(self, position, index=QtCore.QModelIndex()):
+		self.insertColumns(position, self.table_columns_count, index)
+
+	def insertColumns(self, position, columns, parent=QtCore.QModelIndex()):
+		self.beginInsertColumns(parent, position, position + columns - 1)
+		for column in range(0, columns):
+			# mention the column type here example [] (empty list)
+			self.data_column.insert(position, [])
+		self.endInsertColumns()
+		return True
