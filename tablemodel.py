d9b9832de12c88520d1fc07cedf0c6edcd880ec7
Blossom Ghuntla <bghuntla123@gmail.com>
2021-10-25 20:57:56 +0530

    delegate, delegate for a col, model fixes

--- tablemodel.py
+++ tablemodel.py
@@ -2,59 +2,63 @@ from PyQt5 import QtCore
 
 
 class TableModel(QtCore.QAbstractTableModel):
-	def __init__(self, head_columns_names, head_rows_names, table_rows_count=int, table_columns_count=int, parent=None):
-		super(TableModel, self).__init__(parent)
+	def __init__(self, head_columns_names, head_rows_names):
+		super(TableModel, self).__init__()
 
 		self.headColStringsList = head_columns_names
 		self.headRowStringsList = head_rows_names
-		self.data = []  # this is just an empty data to use for row
+		self._data = []  # this is just an empty data to use for row
 		self.data_column = []  # this is just an empty data to use for column
-		self.table_rows_count = table_rows_count
-		self.table_columns_count = table_columns_count
 
 	def rowCount(self, index=QtCore.QModelIndex):
 		# as per the docs this method should return 0 if used for table
-		return len(self.data)
+		# print(self._data)
+		return len(self._data)
 
 	def columnCount(self, index=QtCore.QModelIndex()):
 		# as per the docs this method should return 0 if used for table
+		# print(self.data_column)
 		return len(self.data_column)
 
 	def data(self, index=QtCore.QModelIndex, role=QtCore.Qt.DisplayRole):
 		if index.isValid():
 			if role == QtCore.Qt.DisplayRole or role == QtCore.Qt.EditRole:
-				value = self.data[index.row()][index.column()]
+				value = self._data[index.row()][index.column()]
 				return str(value)
 
 	def setData(self, index=QtCore.QModelIndex, value=QtCore.QVariant, role=QtCore.Qt.EditRole):
 		if role == QtCore.Qt.EditRole:
-			self.data[index.row()][index.column()] = value
+			self._data[index.row()][index.column()] = value
 			return True
 		return False
 
 	def headerData(self, section, orientation=QtCore.Qt.Orientation, role=QtCore.Qt.DisplayRole):
 		if role == QtCore.Qt.DisplayRole:
 			if orientation == QtCore.Qt.Horizontal:
+				# return self._data[section]
 				return self.headColStringsList[section]
 			if orientation == QtCore.Qt.Vertical:
+				# return self._data[section]
 				return self.headRowStringsList[section]
 
 	def flags(self, index):
 		return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable
 
 	def insertRow(self, position, index=QtCore.QModelIndex()):
-		self.insertRows(position, self.table_rows_count, index)
+		# self.insertRows(position, self.table_rows_count, index)
+		self.insertRows(position, 3, index)
 
 	def insertRows(self, position, rows, parent=QtCore.QModelIndex()):
 		self.beginInsertRows(parent, position, position + rows - 1)
 		for row in range(0, rows):
 			# mention rows as per the column count
-			self.data.insert(position, ["", "", "", "", ""])
+			self._data.insert(position, ["", "", "", 0])
 		self.endInsertRows()
 		return True
 
 	def insertColumn(self, position, index=QtCore.QModelIndex()):
-		self.insertColumns(position, self.table_columns_count, index)
+		# self.insertColumns(position, self.table_columns_count, index)
+		self.insertColumns(position, 4, index)
 
 	def insertColumns(self, position, columns, parent=QtCore.QModelIndex()):
 		self.beginInsertColumns(parent, position, position + columns - 1)
